bff94561e61bb8c9a980075e2300643cf942aca2
Snazzah <suggesttosnazzy@gmail.com>
2019-06-11 06:04:52 -0500

    View archive with commands

---
+++ Commands/viewing/cardarchive.js
@@ -0,0 +1,31 @@
+const { Command } = require('faux-classes')
+
+module.exports = class CardArchive extends Command {
+  get name() { return 'cardarchive' }
+  get aliases() { return ['archivedcards'] }
+  get cooldown() { return 2 }
+  get permissions() { return ['auth', 'board'] }
+
+  async exec(message, args, {user}) {
+    let body = await this.client.trello.get.cardsArchived(user.trelloToken, user.current)
+    if(!body.length)
+      return message.reply("There are no found cards in the archive.");
+    await this.client.promptList(message, body, (card, embed) => {
+      if (embed)
+        return `\`${card.shortLink}\` ${card.name}`;
+        else return `${card.shortLink}: ${card.name}`;
+    }, {
+      header: "Use `" + this.client.config.prefix + "opencard <cardID>` to unarchive that card\n" + 
+        "Use `" + this.client.config.prefix + "cardarchive [page]` to iterate this list",
+      pluralName: "Trello Archived Cards",
+      itemsPerPage: 15,
+      startPage: args[0]
+    });
+  }
+
+  get helpMeta() { return {
+    category: 'Viewing',
+    description: 'Lists all the archived cards in the current board.',
+    usage: "[page]"
+  } }
+}
---
+++ Commands/viewing/listarchive.js
@@ -0,0 +1,27 @@
+const { Command } = require('faux-classes')
+
+module.exports = class ListArchive extends Command {
+  get name() { return 'listarchive' }
+  get aliases() { return ['archivedlists'] }
+  get cooldown() { return 2 }
+  get permissions() { return ['auth', 'board'] }
+
+  async exec(message, args, {user}) {
+    let body = await this.client.trello.get.listsArchived(user.trelloToken, user.current)
+    if(!body.length)
+      return message.reply("There are no found cards in the archive.");
+    await this.client.promptList(message, body, list => list.name, {
+      header: "Use `" + this.client.config.prefix + "openlist <listName>` to unarchive that list\n" + 
+        "Use `" + this.client.config.prefix + "listarchive [page]` to iterate this list",
+      pluralName: "Trello Archived Lists",
+      itemsPerPage: 15,
+      startPage: args[0]
+    });
+  }
+
+  get helpMeta() { return {
+    category: 'Viewing',
+    description: 'Lists all the archived lists in the current board.',
+    usage: "[page]"
+  } }
+}
