6c3c6ab0fbd4e8cfeae17f45a76fa130312fa128
Yamboy1 <yamboyd1@gmail.com>
2019-09-20 13:42:30 +1200

    Update index.js

--- Config/index.js
+++ Config/index.js
@@ -1,11 +1,9 @@
-const redis = require("./redis.js");
-const rethink = require("./rethink.js");
+const redis = require("./logins.js");
 const links = require("./links.js");
 const settings = require("./settings.js");
 
 module.exports = {
-    redis,
-    rethink,
+    ...logins
     ...settings,
     ...links
 
