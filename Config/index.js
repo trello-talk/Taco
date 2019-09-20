aeaa9145c0e39b146bb06e470a5f9b38024abd1a
Yamboy1 <yamboyd1@gmail.com>
2019-09-20 13:43:02 +1200

    Update index.js

--- Config/index.js
+++ Config/index.js
@@ -1,10 +1,10 @@
-const redis = require("./logins.js");
+const logins = require("./logins.js");
 const links = require("./links.js");
 const settings = require("./settings.js");
 
 module.exports = {
-    ...logins
-    ...settings,
-    ...links
+    ...logins,
+    ...links,
+    ...settings
 
 }
