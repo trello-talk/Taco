d13c6eb7bae743a4c5dbb651efab13f007d77695
Yamboy1 <yamboyd1@gmail.com>
2019-09-20 13:50:36 +1200

    Update .general.js

--- Config/.general.js
+++ Config/.general.js
@@ -1,8 +1,7 @@
 module.exports = {
-    token:        "" ,
-    prefix:       "" ,
-    elevated:    [""], // These users have access to all admin commands
-    commands:     "" , // The folder which has all the commands
+    token:        "",
+    prefix:       "T!",
+    elevated:    [""],
+    commands:     "./Commands",
     debug:       false,
-    embedColor:  0     // This is the color which is used for embeds
-}
+    embedColor:  4365262
