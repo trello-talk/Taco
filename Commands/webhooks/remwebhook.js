d7e691db7f88b301f39d505234db49ff3ba1d2cd
Yamboy1 <yamboyd1@gmail.com>
2019-09-26 22:37:53 +1200

    remwebhook now removes internal hooks as well

--- Commands/webhooks/remwebhook.js
+++ Commands/webhooks/remwebhook.js
@@ -7,9 +7,17 @@ module.exports = class RemoveWebhook extends Command {
   get aliases() { return ['-webhook', 'delwebhook', 'removewebhook', 'deletewebhook'] }
   get argRequirement() { return 1 }
 
-  async exec(message, args) {
+  async exec(message, args, {user}) {
     try {
+      const { webhookId } = await this.client.data.get.webhookBoard(args[0])
+      const internalWebhooks = await this.client.trello.get.webhooks(user.trelloToken)
+
+      const webhook = internalWebhooks.find(webhook => webhook.id === webhookId);
+      if (!webhook || !webhook.id) throw 404;
+
+      await this.client.trello.delete.webhook(user.trelloToken, webhook.id)
       await this.client.data.delete.webhook(message.guild.id, args[0])
+
       message.reply(`Deleted webhook for board \`${args[0]}\`.`);
     } catch (e) {
       if(e === 404){
