6d92c84094f8642dc7bb813aa6dff0422a21288e
Yamboy1 <yamboyd1@gmail.com>
2019-09-30 11:43:13 +1300

    Add botlist support back in

---
+++ .Config/botlists.js
@@ -0,0 +1,7 @@
+// This file is for bot list api keys. A full list of supported bot lists can be found here: https://github.com/Snazzah/dbots.js#supporting-services
+
+module.exports = {
+    discordbotsorg: "",
+    discordbotsgg:  "",
+    carbon:         "",
+}
