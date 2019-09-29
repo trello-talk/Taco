279192c9c652c557421437dde086c75a5f4d3c4f
Yamboy1 <yamboyd1@gmail.com>
2019-09-30 12:10:12 +1300

    Add better unhandledRejection handling

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const util = require("util");
 const { Faux } = require('faux-core')
 
 let faux = new Faux({ mainDir: __dirname })
 faux.start()
+
+process.on("unhandledRejection", e => {
+  console.log(util.inspect(e));
+});
