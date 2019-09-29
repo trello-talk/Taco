aac8c8fc65869958ad4bb80da5aad68516b7dd29
Yamboy1 <yamboyd1@gmail.com>
2019-09-30 03:33:10 +1300

    Update exec command to allow a mix of stdin/stdout
    This commit does add a new package, so it's necessary to `npm install` after updating

--- Commands/faux-admin/exec.js
+++ Commands/faux-admin/exec.js
@@ -1,17 +1,26 @@
 const { CodeBlock, Command } = require('faux-classes')
+const { command: exec } = require("execa");
 
 module.exports = class Exec extends Command {
   get name() { return 'exec' }
   get aliases() { return ['execute', 'terminal', 'bash'] }
 
-  exec(Message, Args) {
-    Message.channel.startTyping();
-    require("child_process").exec(Args.join(" "), (e, f, r)=>{
-      Message.channel.stopTyping();
-      if(e) return Message.channel.send(CodeBlock.apply(`Error: ${e}`, 'js'));
-      if(r != '') return Message.channel.send(CodeBlock.apply(`STDOUT Error: ${r}`, 'js'));
-      Message.channel.send(CodeBlock.apply(f));
-    });
+  async exec(message, args) {
+    message.channel.startTyping();
+    let output = "";
+    let errMessage = "";
+    try {
+      ({ all: output } = await exec(args.join(" "), { shell: "/bin/bash" }));
+    } catch (e) {
+      if (!e.exitCode) throw e;
+      output = e.all;
+      errMessage = e.message;
+    } finally {
+      message.channel.stopTyping(true);
+      const text = `${errMessage}\n${CodeBlock.apply(output)}`;
+      if (text.length > 2000) return message.channel.send("Output was too long to be displayed!");
+      return message.channel.send(text);
+    }
   }
 
   get permissions() { return ['elevated'] }
