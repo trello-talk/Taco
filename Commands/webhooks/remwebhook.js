d014e2293e3eb23d6a7ce354b9e9af623b278a67
Yamboy1 <yamboyd1@gmail.com>
2019-10-02 09:30:26 +1300

    Fix a bug in remwebhook
    If an internal webhook was deleted, this would cause remwebhook to error. This commit changes this

--- Commands/webhooks/remwebhook.js
+++ Commands/webhooks/remwebhook.js
@@ -11,13 +11,13 @@ module.exports = class RemoveWebhook extends Command {
     try {
       const boardId = await this.client.util.getBoardId(user, args[0]);
       const { webhookId = undefined } = await this.client.data.get.webhookBoard(boardId) || {}
-      if (webhookId === undefined) throw 404;
+      if (webhookId === undefined) return message.reply("Could not find webhook.");
       
-      const internalWebhooks = await this.client.trello.get.webhooks(user.trelloToken)
+      const internalWebhooks = await this.client.trello.get.webhooks(user.trelloToken);
       const webhook = internalWebhooks.find(webhook => webhook.id === webhookId);
-      if (webhook === undefined) throw 404;
+      if (webhook === undefined) await message.channel.send("Internal webhook not found, skipping...");
+      else await this.client.trello.delete.webhook(user.trelloToken, webhook.id)
 
-      await this.client.trello.delete.webhook(user.trelloToken, webhook.id)
       await this.client.data.delete.webhook(message.guild.id, boardId)
 
       message.reply(`Deleted webhook for board \`${boardId}\`.`);
