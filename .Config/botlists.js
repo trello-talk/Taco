4ed63f729c390a1738befb2b42418279306a4e72
Yamboy1 <yamboyd1@gmail.com>
2019-10-01 22:09:26 +1300

    Comment out botlists by default

--- .Config/botlists.js
+++ .Config/botlists.js
@@ -1,7 +1,7 @@
 // This file is for bot list api keys. A full list of supported bot lists can be found here: https://github.com/Snazzah/dbots.js#supporting-services
 
 module.exports = {
-    discordbotsorg: "",
-    // discordbotsgg: "",
+    // discordbotsorg: "",
+    // discordbotsgg:  "",
     // discordbotlist: ""
 }
