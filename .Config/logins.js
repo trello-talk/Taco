734c7db43206045190a39300979feae22525bb8d
Yamboy1 <yamboyd1@gmail.com>
2019-10-04 14:09:50 +1300

    Update formatting in .Config folder

--- .Config/logins.js
+++ .Config/logins.js
@@ -1,19 +1,19 @@
 module.exports = {
-    trello: {
-        token:    "",
-        key:      ""
-    },
-    redis: {
-        host:     "localhost",
-        port:     6379,
-        password: "",
-        prefix:   "trello:"
-    },
-    rethink: {
-        host:     "localhost",
-        port:     28015,
-        user:     "",
-        password: "",
-        database: "Trello"
-    }
+  trello: {
+    token: "",
+    key: ""
+  },
+  redis: {
+    host: "localhost",
+    port: 6379,
+    password: "",
+    prefix: "trello:"
+  },
+  rethink: {
+    host: "localhost",
+    port: 28015,
+    user: "",
+    password: "",
+    database: "Trello"
+  }
 };
