8ecd2b804b12c7e0e311904e29c23f4b11f26fb8
Yamboy1 <yamboyd1@gmail.com>
2019-10-04 14:39:59 +1300

    Update formatting in Classes folder

--- Classes/Data.js
+++ Classes/Data.js
@@ -1,187 +1,237 @@
-	module.exports = (client) => { return {
-	get: {
-		server: function(id){
-			return client.rdb.r.table("servers").get(id).default(null).run(client.rdb.conn);
-		},
-		servers: function(){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("servers").run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					resolve(data.toArray());
-				});
-			});
-		},
-		webhooks: function(){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					resolve(data.toArray());
-				});
-			});
-		},
-		webhooksEdit: function(id, muted){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
-					if (err) { reject(err) }
-					data.each(async (err, row) => {
-						if (err) { reject(err) }
-						if (row.id !== id) return;
-						for (let item of Object.keys(row)) {
-							if (!row[item].bits) return;
-							row[item] = { muted: muted };
-							client.rdb.r.table("webhooks").get(row.id).update(row).run(client.rdb.conn, (err2, data2) => {
-								if(err2){ reject(err2); }
-								resolve(data2);
-							});
-						}
-					})
-				});
-			});
-		},
-		webhooksOf: function(id){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					resolve(data.toArray().map(webhook => {
-						if(webhook[id] !== undefined){
-							return { board: webhook["id"], id: id, bits: webhook[id].bits };
-						}else{
-							return undefined;
-						}
-					}).filter(webhook => {
-						if(webhook !== undefined){
-							return true;
-						}else{
-							return false;
-						}
-					}));
-				});
-			});
-		},
-		webhook: function(id, bid){
-			return client.rdb.r.table("webhooks").get(bid)(id).default(null).run(client.rdb.conn);
-		},
-		webhookBoard: function(bid){
-			return client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn);
-		},
-		user: function(id){
-			return client.rdb.r.table("users").get(id).default(null).run(client.rdb.conn);
-		}
-	},
-	add: {
-		board: function(id, bid){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("servers").get(id).default(null).run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					if(data === null){
-						client.rdb.r.table("servers").insert({ id: id, boards: [], current: bid }).run(client.rdb.conn, (err, data) => {
-							if(err){ reject(err); }
-							resolve(data);
-						});
-					}else{
-						client.rdb.r.table("servers").get(id).update({ boards: client.rdb.r.row("boards").append(bid) }).run(client.rdb.conn, (err2, data2) => {
-							if(err2){ reject(err2); }
-							resolve(data2);
-						});
-					}
-				});
-			});
-		},
-		server: function(data){
-			return client.rdb.r.table("servers").insert(data).run(client.rdb.conn);
-		},
-		webhook: function(id, bid, url, mid, wid){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					if(data === null){
-						let table = { id: bid, modelId: mid};
-						if(wid) table.webhookId = wid;
-						table[id] = { webhook: url, bits: [], muted: false }
-						client.rdb.r.table("webhooks").insert(table).run(client.rdb.conn, (err2, data2) => {
-							if(err2){ reject(err2); }
-							resolve(data2);
-						});
-					}else{
-						let table = { modelId: mid };
-						if(wid) table.webhookId = wid;
-						table[id] = { webhook: url, bits: [] };
-						client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
-							if(err2){ reject(err2); }
-							resolve(data2);
-						});
-					}
-				});
-			});
-		}
-	},
-	setup: {
-		server: function(id, c){
-			return client.rdb.r.table("servers").insert({ id: id, boards: [ c ], current: c }).run(client.rdb.conn);
-		}
-	},
-	delete: {
-		board: function(id, bid){
-            return client.rdb.r.table("servers")
-                .get(id).update({ boards: client.rdb.r.row('boards')
-                .difference([ bid ]) }).default(null).run(client.rdb.conn);
-		},
-		webhook: function(id, bid){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					if(data === null){ reject(404); }
-					let table = data;
-					delete table[id]
-					if(Object.keys(table).length <= 3){
-						client.rdb.r.table("webhooks").get(bid).delete().run(client.rdb.conn, (err2, data2) => {
-							if(err2){ reject(err2); }
-							resolve(data2);
-						});
-					}else{
-						client.rdb.r.table("webhooks").get(bid).replace(table).run(client.rdb.conn, (err2, data2) => {
-							if(err2){ reject(err2); }
-							resolve(data2);
-						});
-					}
-				});
-			});
-		}
-	},
-	set: {
-		server: function(id, data){
-			return client.rdb.r.table("servers").get(id).update(data).run(client.rdb.conn);
-		},
-		user: function(id, data){
-			return client.rdb.r.table("users").get(id).update(data).run(client.rdb.conn);
-		},
-		webhook: function(id, bid, bits){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					if(data === null){ reject(404); }
-					let table = {};
-					table[id] = { bits: bits }
-					client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
-						if(err2){ reject(err2); }
-						resolve(data2);
-					});
-				});
-			});
-		},
-		webhookMute: function(id, bid, muted){
-			return new Promise((resolve, reject) => {
-				client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
-					if(err){ reject(err); }
-					if(data === null){ reject(404); }
-					let table = {};
-					table[id] = { muted: muted }
-					client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
-						if(err2){ reject(err2); }
-						resolve(data2);
-					});
-				});
-			});
-		}
-	}
-}}
+module.exports = (client) => ({
+  get: {
+    server(id) {
+      return client.rdb.r.table("servers").get(id).default(null).run(client.rdb.conn);
+    },
+    servers() {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("servers").run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data.toArray());
+        });
+      });
+    },
+    webhooks() {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data.toArray());
+        });
+      });
+    },
+    webhooksEdit(id, muted) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          data.each(async (err, row) => {
+            if (err) {
+              reject(err);
+            }
+            if (row.id !== id) return;
+            for (let item of Object.keys(row)) {
+              if (!row[item].bits) return;
+              row[item] = { muted: muted };
+              client.rdb.r.table("webhooks").get(row.id).update(row).run(client.rdb.conn, (err2, data2) => {
+                if (err2) {
+                  reject(err2);
+                }
+                resolve(data2);
+              });
+            }
+          });
+        });
+      });
+    },
+    webhooksOf(id) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data.toArray().map(webhook => {
+            if (webhook[id] !== undefined) {
+              return { board: webhook["id"], id: id, bits: webhook[id].bits };
+            } else {
+              return undefined;
+            }
+          }).filter(webhook => {
+            if (webhook !== undefined) {
+              return true;
+            } else {
+              return false;
+            }
+          }));
+        });
+      });
+    },
+    webhook(id, bid) {
+      return client.rdb.r.table("webhooks").get(bid)(id).default(null).run(client.rdb.conn);
+    },
+    webhookBoard(bid) {
+      return client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn);
+    },
+    user(id) {
+      return client.rdb.r.table("users").get(id).default(null).run(client.rdb.conn);
+    }
+  },
+  add: {
+    board(id, bid) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("servers").get(id).default(null).run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data === null) {
+            client.rdb.r.table("servers").insert({
+              id: id,
+              boards: [],
+              current: bid
+            }).run(client.rdb.conn, (err, data) => {
+              if (err) {
+                reject(err);
+              }
+              resolve(data);
+            });
+          } else {
+            client.rdb.r.table("servers").get(id).update({ boards: client.rdb.r.row("boards").append(bid) }).run(client.rdb.conn, (err2, data2) => {
+              if (err2) {
+                reject(err2);
+              }
+              resolve(data2);
+            });
+          }
+        });
+      });
+    },
+    server(data) {
+      return client.rdb.r.table("servers").insert(data).run(client.rdb.conn);
+    },
+    webhook(id, bid, url, mid, wid) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data === null) {
+            let table = { id: bid, modelId: mid };
+            if (wid) table.webhookId = wid;
+            table[id] = { webhook: url, bits: [], muted: false };
+            client.rdb.r.table("webhooks").insert(table).run(client.rdb.conn, (err2, data2) => {
+              if (err2) {
+                reject(err2);
+              }
+              resolve(data2);
+            });
+          } else {
+            let table = { modelId: mid };
+            if (wid) table.webhookId = wid;
+            table[id] = { webhook: url, bits: [] };
+            client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
+              if (err2) {
+                reject(err2);
+              }
+              resolve(data2);
+            });
+          }
+        });
+      });
+    }
+  },
+  setup: {
+    server(id, c) {
+      return client.rdb.r.table("servers").insert({ id: id, boards: [c], current: c }).run(client.rdb.conn);
+    }
+  },
+  delete: {
+    board(id, bid) {
+      return client.rdb.r.table("servers")
+        .get(id).update({
+          boards: client.rdb.r.row("boards")
+            .difference([bid])
+        }).default(null).run(client.rdb.conn);
+    },
+    webhook(id, bid) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data === null) {
+            reject(404);
+          }
+          let table = data;
+          delete table[id];
+          if (Object.keys(table).length <= 3) {
+            client.rdb.r.table("webhooks").get(bid).delete().run(client.rdb.conn, (err2, data2) => {
+              if (err2) {
+                reject(err2);
+              }
+              resolve(data2);
+            });
+          } else {
+            client.rdb.r.table("webhooks").get(bid).replace(table).run(client.rdb.conn, (err2, data2) => {
+              if (err2) {
+                reject(err2);
+              }
+              resolve(data2);
+            });
+          }
+        });
+      });
+    }
+  },
+  set: {
+    server(id, data) {
+      return client.rdb.r.table("servers").get(id).update(data).run(client.rdb.conn);
+    },
+    user(id, data) {
+      return client.rdb.r.table("users").get(id).update(data).run(client.rdb.conn);
+    },
+    webhook(id, bid, bits) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data === null) {
+            reject(404);
+          }
+          let table = {};
+          table[id] = { bits: bits };
+          client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
+            if (err2) {
+              reject(err2);
+            }
+            resolve(data2);
+          });
+        });
+      });
+    },
+    webhookMute(id, bid, muted) {
+      return new Promise((resolve, reject) => {
+        client.rdb.r.table("webhooks").get(bid).default(null).run(client.rdb.conn, (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          if (data === null) {
+            reject(404);
+          }
+          let table = {};
+          table[id] = { muted: muted };
+          client.rdb.r.table("webhooks").get(bid).update(table).run(client.rdb.conn, (err2, data2) => {
+            if (err2) {
+              reject(err2);
+            }
+            resolve(data2);
+          });
+        });
+      });
+    }
+  }
+});
--- Classes/Util.js
+++ Classes/Util.js
@@ -1,51 +1,51 @@
 const fuzzy = require("fuzzy");
 const StickerEmojis = {
-	thumbsup: "<:trelloThumbsUp:245756307456524299>",
-	thumbsdown: "<:trelloThumbsDown:245756307452329984>",
-	heart: "<:trelloHeart:245752268312281109>",
-	star: "<:trelloStar:245756307578028034>",
-	clock: "<:trelloClock:245758457100238848>",
-	huh: "<:trelloHuh:245756306936299521>",
-	rocketship: "<:trelloRocketship:245756307309592576>",
-	warning: "<:trelloWarning:245758457171410945>",
-	smile: "<:trelloSmile:245756307334889472>",
-	laugh: "<:trelloLaugh:245756307221643274>",
-	frown: "<:trelloFrown:322123093424209921>",
-	check: "<:trelloCheck:245758456814895105>",
+  thumbsup: "<:trelloThumbsUp:245756307456524299>",
+  thumbsdown: "<:trelloThumbsDown:245756307452329984>",
+  heart: "<:trelloHeart:245752268312281109>",
+  star: "<:trelloStar:245756307578028034>",
+  clock: "<:trelloClock:245758457100238848>",
+  huh: "<:trelloHuh:245756306936299521>",
+  rocketship: "<:trelloRocketship:245756307309592576>",
+  warning: "<:trelloWarning:245758457171410945>",
+  smile: "<:trelloSmile:245756307334889472>",
+  laugh: "<:trelloLaugh:245756307221643274>",
+  frown: "<:trelloFrown:322123093424209921>",
+  check: "<:trelloCheck:245758456814895105>",
 
-	"pete-alert": "<:trellopeteAlert:245758456840060941>",
-	"pete-award": "<:trellopeteAward:245758457272205312>",
-	"pete-broken": "<:trellopeteBroken:322128049669668864>",
-	"pete-busy": "<:trellopeteBusy:322128049715937280>",
-	"pete-completed": "<:trellopeteComplete:245758456999575553>",
-	"pete-confused": "<:trellopeteConfused:322128049686708224>",
-	"pete-ghost": "<:trellopeteGhost:322128049707417610>",
-	"pete-happy": "<:trellopeteHappy:322128049678319616>",
-	"pete-love": "<:trellopeteLove:322128049795497994>",
-	"pete-music": "<:trellopeteMusic:322128049774526465>",
-	"pete-shipped": "<:trellopeteShipped:322128050148081674>",
-	"pete-sketch": "<:trellopeteSketch:322128049837703178>",
-	"pete-space": "<:trellopeteSpace:322128050185568276>",
-	"pete-talk": "<:trellopeteTalk:322128050231967744>",
-	"pete-vacation": "<:trellopeteVacation:322128050042961921>",
+  "pete-alert": "<:trellopeteAlert:245758456840060941>",
+  "pete-award": "<:trellopeteAward:245758457272205312>",
+  "pete-broken": "<:trellopeteBroken:322128049669668864>",
+  "pete-busy": "<:trellopeteBusy:322128049715937280>",
+  "pete-completed": "<:trellopeteComplete:245758456999575553>",
+  "pete-confused": "<:trellopeteConfused:322128049686708224>",
+  "pete-ghost": "<:trellopeteGhost:322128049707417610>",
+  "pete-happy": "<:trellopeteHappy:322128049678319616>",
+  "pete-love": "<:trellopeteLove:322128049795497994>",
+  "pete-music": "<:trellopeteMusic:322128049774526465>",
+  "pete-shipped": "<:trellopeteShipped:322128050148081674>",
+  "pete-sketch": "<:trellopeteSketch:322128049837703178>",
+  "pete-space": "<:trellopeteSpace:322128050185568276>",
+  "pete-talk": "<:trellopeteTalk:322128050231967744>",
+  "pete-vacation": "<:trellopeteVacation:322128050042961921>",
 
-	"taco-active": "<:trellotacoActive:322130958868545536>",
-	"taco-alert": "<:trellotacoAlert:322130958675607553>",
-	"taco-angry": "<:trellotacoAngry:322130959023603712>",
-	"taco-celebrate": "<:trellotacoCelebrate:322130958834860034>",
-	"taco-clean": "<:trellotacoClean:322130958692384770>",
-	"taco-confused": "<:trellotacoConfused:322130959342370818>",
-	"taco-cool": "<:trellotacoCool:322130959191375873>",
-	"taco-embarrassed": "<:trellotacoEmbarrassed:322130959325593600>",
-	"taco-love": "<:trellotacoLove:322144522752753665>",
-	"taco-money": "<:trellotacoMoney:322130959145107467>",
-	"taco-pixel": "<:trellotacoPixel:322130959363342336>",
-	"taco-proto": "<:trellotacoProto:322130959468331008>",
-	"taco-reading": "<:trellotacoReading:322130959313141771>",
-	"taco-robot": "<:trellotacoRobot:322130959510142976>",
-	"taco-sleeping": "<:trellotacoSleeping:322130959585509376>",
-	"taco-trophy": "<:trellotacoTrophy:322130959967453194>"
-}
+  "taco-active": "<:trellotacoActive:322130958868545536>",
+  "taco-alert": "<:trellotacoAlert:322130958675607553>",
+  "taco-angry": "<:trellotacoAngry:322130959023603712>",
+  "taco-celebrate": "<:trellotacoCelebrate:322130958834860034>",
+  "taco-clean": "<:trellotacoClean:322130958692384770>",
+  "taco-confused": "<:trellotacoConfused:322130959342370818>",
+  "taco-cool": "<:trellotacoCool:322130959191375873>",
+  "taco-embarrassed": "<:trellotacoEmbarrassed:322130959325593600>",
+  "taco-love": "<:trellotacoLove:322144522752753665>",
+  "taco-money": "<:trellotacoMoney:322130959145107467>",
+  "taco-pixel": "<:trellotacoPixel:322130959363342336>",
+  "taco-proto": "<:trellotacoProto:322130959468331008>",
+  "taco-reading": "<:trellotacoReading:322130959313141771>",
+  "taco-robot": "<:trellotacoRobot:322130959510142976>",
+  "taco-sleeping": "<:trellotacoSleeping:322130959585509376>",
+  "taco-trophy": "<:trellotacoTrophy:322130959967453194>"
+};
 
 const TrelloEvents = {
   voteOnCard: "a card has been voted on",
@@ -81,47 +81,47 @@ const TrelloEvents = {
   updateCustomField: "a custom field has been updated",
   copyCard: "a card has been duplicated",
   updateChecklist: "a checklist has been renamed"
-}
+};
 
-module.exports = (client) => { 
+module.exports = (client) => {
   let Util = {
     StickerEmojis, TrelloEvents,
     layout: {
       cardLabels(labels) {
-        return labels.map(label => "["+label.name+"]["+Util.capFirst(label.color)+"]")
+        return labels.map(label => "[" + label.name + "][" + Util.capFirst(label.color) + "]");
       },
       cardLabelsEmbed(labels) {
-        return labels.map(label => label.name + ", Color: " + Util.capFirst(label.color))
+        return labels.map(label => label.name + ", Color: " + Util.capFirst(label.color));
       },
       attachments(atchmts) {
-        return atchmts.map(atchmt => atchmt.url)
+        return atchmts.map(atchmt => atchmt.url);
       },
       members(members) {
-        return members.map(member => member.fullName)
+        return members.map(member => member.fullName);
       },
       stickers(stickers) {
-        var arr = []
-        for(var a in stickers){
-          if(!StickerEmojis[stickers[a].image]) return;
-          arr.push(StickerEmojis[stickers[a].image])
+        var arr = [];
+        for (var a in stickers) {
+          if (!StickerEmojis[stickers[a].image]) return;
+          arr.push(StickerEmojis[stickers[a].image]);
         }
-        var obj = { }
+        var obj = {};
         for (var i = 0, j = arr.length; i < j; i++) {
-           obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+          obj[arr[i]] = (obj[arr[i]] || 0) + 1;
         }
-        var obj2 = []
+        var obj2 = [];
         for (var emoji in obj) {
-          amt = obj[emoji]
-          obj2.push(emoji+' '+amt)
+          amt = obj[emoji];
+          obj2.push(emoji + " " + amt);
         }
-        return obj2
+        return obj2;
       }
     },
     filter(text) {
-      return text.toString().replace(client.apiKey, "🔑").replace(client.apiToken, "🔶")
+      return text.toString().replace(client.apiKey, "🔑").replace(client.apiToken, "🔶");
     },
     async filterStatus(res) {
-      switch(res.status){
+      switch (res.status) {
         case 404:
           throw "Not Found";
         case 400:
@@ -141,29 +141,29 @@ module.exports = (client) => {
       }
     },
     sendError(message, e) {
-        message.channel.stopTyping();
-        if (e.errorCode) return Util.sendWebError(message, e);
-        client.log("Command Error:\n", e);
-        message.channel.send(`Error! Report in the Support Server listed in \`${client.config.prefix}help\`!\n\`\`\`js\n${Util.filter(e.stack)}\`\`\``)
+      message.channel.stopTyping();
+      if (e.errorCode) return Util.sendWebError(message, e);
+      client.log("Command Error:\n", e);
+      message.channel.send(`Error! Report in the Support Server listed in \`${client.config.prefix}help\`!\n\`\`\`js\n${Util.filter(e.stack)}\`\`\``);
     },
     sendWebError(message, e) {
-      switch(e.errorCode){
+      switch (e.errorCode) {
         case "err":
           console.log("Web Error:\n", e);
-          message.channel.send(`There was an error processing that command! Report in the Support Server listed in \`${client.config.prefix}help\`!\n\`${Util.filter(e.err.text)}\``)
-        break;
+          message.channel.send(`There was an error processing that command! Report in the Support Server listed in \`${client.config.prefix}help\`!\n\`${Util.filter(e.err.text)}\``);
+          break;
         case "statusfail":
           console.log("Web Status Fail:\n", e);
-          message.channel.send(`I could not do that! \`${e.response.status}: ${e.errorText}\``)
-        break;
+          message.channel.send(`I could not do that! \`${e.response.status}: ${e.errorText}\``);
+          break;
         default:
-          if(e.stack){
+          if (e.stack) {
             message.channel.send(`Error! Report in the Support Server listed in \`${client.config.prefix}help\`!\n\`\`\`js\n${Util.filter(e.stack)}\`\`\``);
-          }else{
-            message.channel.send(`UNKNOWN ERR, CHECK CONSOLE`)
-            console.log(e)
+          } else {
+            message.channel.send(`UNKNOWN ERR, CHECK CONSOLE`);
+            console.log(e);
           }
-        break;
+          break;
       }
     },
     rInt(min, max) {
@@ -173,51 +173,57 @@ module.exports = (client) => {
       return str[0].toUpperCase() + str.slice(1);
     },
     checkPerm(user, server) {
-      if(server.owner.user.id == user.id || client.config.elevated.includes(user.id)) return true;
-      return server.member(user).roles.array().map(r => r.name.toLowerCase().startsWith('trello')).includes(true)
+      if (server.owner.user.id == user.id || client.config.elevated.includes(user.id)) return true;
+      return server.member(user).roles.array().map(r => r.name.toLowerCase().startsWith("trello")).includes(true);
     },
     titleCase(str) {
-      words = str.toLowerCase().split(' ');
+      words = str.toLowerCase().split(" ");
 
-      for(var i = 0; i < words.length; i++) {
-            var letters = words[i].split('');
-            letters[0] = letters[0].toUpperCase();
-            words[i] = letters.join('');
+      for (var i = 0; i < words.length; i++) {
+        var letters = words[i].split("");
+        letters[0] = letters[0].toUpperCase();
+        words[i] = letters.join("");
       }
-      return words.join(' ');
+      return words.join(" ");
     },
     toHHMMSS(str) {
       var sec_num = parseInt(str, 10); // don't forget the second param
-      var hours   = Math.floor(sec_num / 3600);
+      var hours = Math.floor(sec_num / 3600);
       var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
       var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-      if (hours   < 10) {hours   = "0" + hours;}
-      if (minutes < 10) {minutes = "0" + minutes;}
-      if (seconds < 10) {seconds = "0" + seconds;}
-      var time    = hours + ':' + minutes + ':' + seconds;
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      var time = hours + ":" + minutes + ":" + seconds;
       return time;
     },
     splitArray(array, chunk) {
       let finalarray = [];
-      let i,j,temparray;
-      for (i=0,j=array.length; i<j; i+=chunk) {
-        temparray = array.slice(i,i+chunk);
+      let i, j, temparray;
+      for (i = 0, j = array.length; i < j; i += chunk) {
+        temparray = array.slice(i, i + chunk);
         finalarray.push(temparray);
       }
       return finalarray;
     },
-    pageNumber(ipp, count, page = 1){
+    pageNumber(ipp, count, page = 1) {
       let p = 1;
-      let max = Math.ceil(count/ipp)
-      if(Number(page)){
+      let max = Math.ceil(count / ipp);
+      if (Number(page)) {
         p = Number(page);
-        if(p < 1) p = 1;
-        if(p > max) p = max;
+        if (p < 1) p = 1;
+        if (p > max) p = max;
       }
-      return [p, max]
+      return [p, max];
     },
-    qSearch(items, item, key = 'name'){
+    qSearch(items, item, key = "name") {
       return fuzzy.filter(item, items, {
         extract: el => el[key]
       }).map(el => el.original);
@@ -228,32 +234,32 @@ module.exports = (client) => {
       });
     },
     async query(cxtMessage, items, query, key = undefined, displayItem = undefined, promptText = undefined) {
-      let results = Util.qSearch(items, query, key)
-      let result = null
-      if(results.length == 1)
+      let results = Util.qSearch(items, query, key);
+      let result = null;
+      if (results.length == 1)
         result = results[0];
-        else if(results.length > 1) {
-          let promptResult = await client.prompt(cxtMessage, results, displayItem, promptText)
-          if(promptResult === null) return { quit: true };
-          result = promptResult;
-        }
-      return { results, result }
+      else if (results.length > 1) {
+        let promptResult = await client.prompt(cxtMessage, results, displayItem, promptText);
+        if (promptResult === null) return { quit: true };
+        result = promptResult;
+      }
+      return { results, result };
     },
     linkList(array) {
-      return array.reduce((acc, value) => `${acc}  • **<${value}>**\n`, "")
+      return array.reduce((acc, value) => `${acc}  • **<${value}>**\n`, "");
     },
     async getBoardId(user, input) {
       if (!input.startsWith) return null;
 
       let { boards = [] } = await client.trello.get.boards(user.trelloToken, user.trelloID);
       const board = boards.find(board => input.startsWith(board.shortLink) || input.startsWith(board.shortUrl));
-      
+
       if (board === undefined) return null;
       return board.shortLink;
     },
     getCardId(...args) {
       return this.getBoardId(...args);
     }
-  }
-  return Util
+  };
+  return Util;
 };
