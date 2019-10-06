bc825e80c1b628ade369576240b5b836b7afbd6b
Yamboy1 <yamboyd1@gmail.com>
2019-10-06 13:04:43 +1300

    Remove checks from auth command
    These checks have been built into turquoise instead

--- Commands/general/auth.js
+++ Commands/general/auth.js
@@ -4,11 +4,8 @@ module.exports = class Auth extends Command {
   get name() { return "auth"; }
   get cooldown() { return 0; }
 
-  async exec(message) {
-    const userId = message.author.id;
-    const currentAuth = await this.client.data.get.user(userId);
-    if (currentAuth === null) return message.channel.send(`Authorize your Trello account with your Discord here: **<${this.client.config.authURL}>**`);
-    await message.reply("Your account is already authorized with Trello! To connect to a different Trello account, please clear your current authorization with `T!clearauth`, and run this command again")
+  exec(message) {
+    return message.channel.send(`Authorize your Trello account with your Discord here: **<${this.client.config.authURL}>**`);
   }
 
   get helpMeta() {
