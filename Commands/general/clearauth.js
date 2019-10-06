44db17dc6e2708cb18d30007ea505df9b4634b01
Yamboy1 <yamboyd1@gmail.com>
2019-10-06 19:25:34 +1300

    Remove need for try...catch at all

--- Commands/general/clearauth.js
+++ Commands/general/clearauth.js
@@ -9,20 +9,17 @@ module.exports = class ClearAuth extends Command {
     const currentAuth = await this.client.data.get.user(userId);
     if (currentAuth === null) return message.reply('Your account is not currently authorized with Trello!');
     await message.channel.send('Are you sure you would like to clear your authorization? You will need to reauthorize again to use trello commands. (Type `yes` to continue)');
-    try {
-      const filter = m => m.author.id === userId;
-      const messages = await message.channel.awaitMessages(filter, {
-        max: 1,
-        time: 30000,
-        errors: ['time']
-      });
-      if (messages.first().content.toLowerCase() !== 'yes') {
-        return message.channel.send('Cancelled!');
-      }
-    } catch(error) {
+
+    const filter = m => m.author.id === userId;
+    const messages = await message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 30000,
+    });
+
+    if (!messages.size || messages.first().content.toLowerCase() !== 'yes') {
       return message.channel.send('Cancelled!');
-      console.log('Error occured during clearauth command: \n', error);
     }
+
     await this.client.data.delete.user(userId);
     await message.channel.send('Auth cleared!');
   }
