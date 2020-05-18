f50ee5d34520766f9a263b028f91fdf2515e0fca
Snazzah <suggesttosnazzy@gmail.com>
2020-05-18 10:17:26 -0500

    fix(core): Hotfix elevated user cooldown

--- src/commandloader.js
+++ src/commandloader.js
@@ -106,7 +106,7 @@ module.exports = class CommandLoader {
    * @param {Command} command
    */
   async processCooldown(message, command) {
-    if (message.author.id === this.client.config.owner) return true;
+    if (this.client.config.elevated.includes(message.author.id)) return true;
     const now = Date.now() - 1;
     const cooldown = command.cooldownAbs;
     let userCD = await this.client.db.hget(`cooldowns:${message.author.id}`, command.name) || 0;
