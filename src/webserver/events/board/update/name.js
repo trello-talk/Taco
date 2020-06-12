9c87e4f97d9f08eb3c51fb127d98594409948e56
Snazzah <suggesttosnazzy@gmail.com>
2020-06-12 10:19:03 -0500

    fix(webserver): Fix invalid translation string

--- src/webserver/events/board/update/name.js
+++ src/webserver/events/board/update/name.js
@@ -22,7 +22,7 @@ exports.name = 'UPDATE_BOARD_NAME';
 exports.exec = async data => {
   const _ = data.localeModule;
   return data.send({
-    title: _('webhooks.rename_board', {
+    title: _('webhooks.board_rename', {
       member: data.invoker.webhookSafeName,
       board: data.util.cutoffText(data.board.name, 50),
       oldName: data.util.cutoffText(data.oldData.name, 50)
