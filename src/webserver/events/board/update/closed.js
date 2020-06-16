9e53d6bfc251468cf10c3de0039ee924020ae4a7
Snazzah <suggesttosnazzy@gmail.com>
2020-06-16 14:17:28 -0500

    fix(webserver): hotfix board close event

--- src/webserver/events/board/update/closed.js
+++ src/webserver/events/board/update/closed.js
@@ -22,8 +22,8 @@ exports.name = 'UPDATE_BOARD_CLOSED';
 exports.exec = async data => {
   const _ = data.localeModule;
   return data.send({
-    title: _(data.card.closed ?
-      'webhooks.archive_card' : 'webhooks.unarchive_card', {
+    title: _(data.board.closed ?
+      'webhooks.archive_board' : 'webhooks.unarchive_board', {
       member: data.invoker.webhookSafeName,
       board: data.util.cutoffText(data.board.name, 50)
     }),
