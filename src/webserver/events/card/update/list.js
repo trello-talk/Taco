0085da96c4b66fc5202a1b981d485c95a5e4b7f2
Snazzah <suggesttosnazzy@gmail.com>
2020-08-09 12:03:08 -0500

    Update license to MIT

--- src/webserver/events/card/update/list.js
+++ src/webserver/events/card/update/list.js
@@ -1,22 +1,3 @@
-/*
- This file is part of TrelloBot.
- Copyright (c) Snazzah 2016 - 2019
- Copyright (c) Trello Talk Team 2019 - 2020
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 exports.name = 'UPDATE_CARD_LIST';
 
 exports.exec = async data => {
