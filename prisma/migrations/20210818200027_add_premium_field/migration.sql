dee842a4882c4c457ed064e49c1f14d260283e11
Snazzah <suggesttosnazzy@gmail.com>
2021-12-22 11:13:52 -0600

    chore: rollback prisma migration

--- prisma/migrations/20210818200027_add_premium_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "servers" ADD COLUMN     "premium" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "premium" BOOLEAN NOT NULL DEFAULT false;
